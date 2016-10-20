6dbb31ea304535b3aaf7201cf91d2026d447895a
Steven Hiscocks <steven@hiscocks.me.uk>
2016-10-20 20:20:29 +0100

    Increment version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='yidashcam',
-    version='0.6.2',
+    version='0.7',
     description='An unofficial python library for interfacing with the Xiaomi YI Dash Cam.',
     long_description=long_description,
     url='https://github.com/kwirk/yidashcam',
--- yidashcam/__init__.py
+++ yidashcam/__init__.py
@@ -1,7 +1,7 @@
 """Unofficial module for interacting with Xiaomi YI Dashcam"""
 
 __author__ = "Steven Hiscocks"
-__version__ = "0.6.2"
+__version__ = "0.7"
 
 import datetime
 import enum
